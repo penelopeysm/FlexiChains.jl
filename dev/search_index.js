76a4732f1d8c509c78f3e5ee72732913f24ece44
Documenter.jl <documenter@juliadocs.github.io>
2025-07-10 23:40:15 +0000

    build based on c13acfb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FlexiChains","page":"Home","title":"FlexiChains","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Yada.","category":"page"},{"location":"#FlexiChains.greet","page":"Home","title":"FlexiChains.greet","text":"greet()\n\nPrints a greeting message to the console.\n\n\n\n\n\n","category":"function"}]
+}
