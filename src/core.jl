e7472d598741c00080352f99e5585f9da54f0bcd
Penelope Yong <penelopeysm@gmail.com>
2025-07-11 02:31:28 +0100

    Flesh out some basic types

--- src/core.jl
+++ src/core.jl
@@ -6,18 +6,25 @@ using StaticArrays: SMatrix
 
 export FlexiChain
 
-function to_smatrix_with_check(
-    key, mat::AbstractMatrix, niter::Int, nchains::Int
-)
+const NTOf{T} = NamedTuple{<:Any,<:NTuple{<:Any,<:T}} where {T}
+
+"""
+    to_smatrix_with_size_check(key_name::String, mat::AbstractMatrix, niter::Int, nchains::Int)
+
+Convert an AbstractMatrix to an `SMatrix{niter,nchains}`. Error if the size is wrong.
+"""
+function to_smatrix_with_size_check(
+    key_name::String, mat::AbstractMatrix{T}, niter::Int, nchains::Int
+)::SMatrix{niter,nchains,T} where {T}
     if size(mat) != (niter, nchains)
-        msg = "The data for key $key had an inconsistent size: expected ($(niter), $(nchains)), got $(size(mat))."
+        msg = "The data for $key_name had an inconsistent size: expected ($(niter), $(nchains)), got $(size(mat))."
         throw(ArgumentError(msg))
     end
     return SMatrix{niter,nchains}(mat)
 end
 
 """
-    FlexiChain{TKey,NIter,NChains}
+    FlexiChain{TKey,NIter,NChains,Sections}
 
 TODO: Document further.
 
@@ -26,65 +33,60 @@ of the number of iterations and chains. This allows us to have compile-time
 guarantees that the sizes of the arrays are constant across all parameters
 (which must necessarily be true for a Markov chain).
 """
-struct FlexiChain{TKey,NIter,NChains} <: AbstractMCMC.AbstractChains
-    params::Dict{TKey,SMatrix{NIter,NChains,<:AbstractFloat}}
-    core::Dict{TKey,SMatrix{NIter,NChains}}
-    other::Dict{Tuple{TKey,Symbol},SMatrix{NIter,NChains}}
+struct FlexiChain{TKey,NIter,NChains,Sections} <: AbstractMCMC.AbstractChains
+    # all parameters must share a common type; their values must be real numbers
+    parameters::Dict{TKey,SMatrix{NIter,NChains,<:Real}}
+    # section names are always Symbol; but the underlying keys (and values) can
+    # be anything
+    other_sections::NTOf{Dict{<:Any,<:SMatrix{NIter,NChains}}}
 
-    # Constructor that doesn't require StaticArrays.
     function FlexiChain{TKey}(
-        params::AbstractDict{<:Any,<:AbstractMatrix{<:AbstractFloat}},
-        core::AbstractDict{<:Any,<:AbstractMatrix},
-        other::AbstractDict{<:Any,<:AbstractMatrix},
+        parameters::AbstractDict{TKey,<:AbstractMatrix{<:Real}},
+        other_sections::NTOf{AbstractDict{<:Any,<:AbstractMatrix}},
     ) where {TKey}
-        # Check if they're all empty
-        if isempty(params) && isempty(core) && isempty(other)
-            return FlexiChain{TKey,0,0}(
-                Dict{TKey,SMatrix{0,0,<:AbstractFloat}}(),
-                Dict{TKey,SMatrix{0,0}}(),
-                Dict{Tuple{TKey,Symbol},SMatrix{0,0}},
-            )
-        end
-
         # Extract the number of iterations and chains from any of the data matrices
-        first_matrix = if isempty(params)
-            if isempty(core)
-                first(other).second
+        first_matrix = if isempty(parameters)
+            if isempty(other_sections) || all(isempty, values(other_sections))
+                # If they're all empty we can return early
+                return FlexiChain{TKey,0,0}(
+                    Dict{TKey,SMatrix{0,0,float(Real)}}(),
+                    Dict{Tuple{Symbol,Any},SMatrix{0,0}},
+                )
             else
-                first(core).second
+                # Get the first non-empty matrix from `other`
+                first(merge(values(other_sections)...))
             end
         else
-            first(params).second
+            first(parameters).second
         end
         niter, nchains = size(first_matrix)
 
         # Check that all matrices have the same size
-        params_sdict = Dict(
-            (k, to_smatrix_with_check(k, v, niter, nchains)) for (k, v) in pairs(params)
+        params_checked = Dict(
+            (k, to_smatrix_with_size_check("parameter $k", v, niter, nchains)) for
+            (k, v) in pairs(parameters)
         )
-        core_sdict = Dict(
-            (k, to_smatrix_with_check(k, v, niter, nchains)) for (k, v) in pairs(core)
-        )
-        other_sdict = Dict(
-            (k, to_smatrix_with_check(k, v, niter, nchains)) for (k, v) in pairs(other)
-        )
-        return new{TKey,niter,nchains}(params_sdict, core_sdict, other_sdict)
+        other_checked = NamedTuple([
+            s => Dict(
+                (k, to_smatrix_with_size_check("key $k in section $s", v, niter, nchains)) for (k, v) in pairs(d)
+            ) for (s, d) in pairs(other_sections)
+        ])
+        section_names = tuple(keys(other_checked)...)
+        return new{TKey,niter,nchains,section_names}(params_checked, other_checked)
     end
 end
 
 function Base.size(chain::FlexiChain{TKey,NIter,NChains}) where {TKey,NIter,NChains}
-    num_objects = length(chain.params) + length(chain.core) + length(chain.other)
+    num_objects = length(chain.parameters) + length(chain.other_sections)
     return (NIter, num_objects, NChains)
 end
 
 function get_parameter(chain::FlexiChain{TKey}, key::TKey) where {TKey}
-    return get(chain.params, key, nothing)
+    return get(chain.parameters, key, nothing)
 end
 
-function get_core(chain::FlexiChain{TKey}, key::TKey) where {TKey}
-    return get(chain.core, key, nothing)
-end
-
-function get_other(chain::FlexiChain{TKey}, key::TKey, section_name::Symbol) where {TKey}
-    return get(chain.other, (key, section_name), nothing)
+function get_other(chain::FlexiChain, section_name::Symbol, key::Any)
+    section = get(chain.other_sections, section_name, nothing)
+    section === nothing && return nothing
+    return get(section, key, nothing)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,17 @@ using FlexiChains: FlexiChains, FlexiChain
 using Test
 
 @testset "FlexiChains.jl" begin
-
     @testset "core.jl" begin
         @testset "Correct" begin
             params_data = Dict{Symbol,Matrix{Float64}}(
                 :param1 => rand(100, 3), :param2 => rand(100, 3)
             )
-            core_data = Dict{Symbol,Matrix{Float64}}(
-                :core1 => rand(100, 3), :core2 => rand(100, 3)
+            other_data = (;
+                section1=Dict{Symbol,Matrix{Float64}}(
+                    :other1 => rand(100, 3), :other2 => rand(100, 3)
+                )
             )
-            other_data = Dict{Tuple{Symbol,Symbol},Matrix{Float64}}(
-                (:other1, :section1) => rand(100, 3), (:other2, :section2) => rand(100, 3)
-            )
-            chain = FlexiChain{Symbol}(params_data, core_data, other_data)
+            chain = FlexiChain{Symbol}(params_data, other_data)
             @test chain isa FlexiChain
         end
 
@@ -22,11 +20,7 @@ using Test
             params_data = Dict{Symbol,Matrix{Float64}}(
                 :param1 => rand(100, 2), :param2 => rand(100, 3)
             )
-            core_data = Dict{Symbol,Matrix{Float64}}()
-            other_data = Dict{Tuple{Symbol,Symbol},Matrix{Float64}}()
-            @test_throws ArgumentError FlexiChain{Symbol}(
-                params_data, core_data, other_data
-            )
+            @test_throws ArgumentError FlexiChain{Symbol}(params_data, NamedTuple())
         end
 
         @testset "getters" begin
@@ -34,14 +28,12 @@ using Test
             y_data = rand(100, 3)
             some_other_stuff = fill("hello", 100, 3)
             params_data = Dict{Symbol,Matrix{Float64}}(:x => x_data, :y => y_data)
-            core_data = Dict{Symbol,Matrix}(:somestuff => some_other_stuff)
-            other_data = Dict{Tuple{Symbol,Symbol},Matrix{Float64}}()
-            chain = FlexiChain{Symbol}(params_data, core_data, other_data)
+            other_data = (; section1=Dict{Symbol,Matrix}(:somestuff => some_other_stuff))
+            chain = FlexiChain{Symbol}(params_data, other_data)
 
-            FlexiChains.get_parameter(chain, :x) == x_data
-            FlexiChains.get_parameter(chain, :y) == y_data
-            FlexiChains.get_core(chain, :somestuff) == some_other_stuff
+            @test FlexiChains.get_parameter(chain, :x) == x_data
+            @test FlexiChains.get_parameter(chain, :y) == y_data
+            @test FlexiChains.get_other(chain, :section1, :somestuff) == some_other_stuff
         end
     end
-
 end
