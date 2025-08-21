70c51dd17e7ef9dde82a5fcde24df7b7d0c04951
Penelope Yong <penelopeysm@gmail.com>
2025-08-21 01:35:13 +0100

    Add AbstractPPL as dep
    Closes #1

--- src/FlexiChains.jl
+++ src/FlexiChains.jl
@@ -6,4 +6,9 @@ include("data_structures.jl")
 include("interface.jl")
 include("display.jl")
 
+# For maximum ease of use with Turing...
+using AbstractPPL: VarName, @varname
+const VNFlexiChain = FlexiChain{VarName}
+export VarName, @varname, VNFlexiChain
+
 end # module FlexiChains
