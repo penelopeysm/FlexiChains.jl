51bbd4a7c26b3ae4a152326396c87d3c6c2377a3
simonsteiger <simon.j.steiger@gmail.com>
2026-04-23 19:51:57 +0200

    test: add Plots.jl reference tests

---
+++ test/ext/pairplots.jl
@@ -0,0 +1,37 @@
+module FlexiChainsPairPlotsExtRefTests
+
+using CairoMakie
+using PairPlots
+using FlexiChains: FlexiChain, Parameter
+using StableRNGs: StableRNG
+using Test
+
+include("../reference_tests_utils.jl")
+
+function make_test_chain()
+    rng = StableRNG(42)
+    N_iters = 100
+    N_chains = 2
+    dicts = [
+        Dict(
+            Parameter(:a) => randn(rng),
+            Parameter(:b) => randn(rng),
+        )
+        for _ in 1:N_iters, _ in 1:N_chains
+    ]
+    return FlexiChain{Symbol}(N_iters, N_chains, dicts)
+end
+
+@testset "PairPlots reference tests" begin
+    chn = make_test_chain()
+
+    reftest("pairplot") do
+        PairPlots.pairplot(chn; pool_chains=false)
+    end
+
+    reftest("pairplot_pooled") do
+        PairPlots.pairplot(chn; pool_chains=true)
+    end
+end
+
+end # module
