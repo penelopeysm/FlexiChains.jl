5b53b960f3ea1a12a15468691f97da7ab1e14b33
Penelope Yong <penelopeysm@gmail.com>
2026-07-23 10:00:05 +0800

    remove

---
+++ versions.js
@@ -0,0 +1,13 @@
+var DOC_VERSIONS = [
+  "dev",
+  "stable",
+  "v0.6",
+  "v0.5",
+  "v0.4",
+  "v0.3",
+  "v0.2",
+  "v0.1",
+  "v0.0.3",
+];
+var DOCUMENTER_NEWEST = "0.6";
+var DOCUMENTER_STABLE = "stable";
